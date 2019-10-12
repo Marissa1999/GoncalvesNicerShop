757d8315d323228de92e46d49bc3c4c8f8e47162
Marissa Gonçalves <marissagoncalves13@gmail.com>
2019-10-12 11:47:35 -0400

    Added more documentation to the ProductAdapter class.

--- app/src/main/java/com/example/goncalvesnicershop/ProductAdapter.java
+++ app/src/main/java/com/example/goncalvesnicershop/ProductAdapter.java
@@ -103,8 +103,8 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             this.mAlbumPrice = albumItemView.findViewById(R.id.album_price);
             this.mAlbumQuantity = albumItemView.findViewById(R.id.album_quantity);
             this.mAlbumSubtotal = albumItemView.findViewById(R.id.album_subtotal);
-            this.mAdditionButton = albumItemView.findViewById(R.id.adding_button);
-            this.mSubtractionButton = albumItemView.findViewById(R.id.minus_button);
+            this.mAdditionButton = albumItemView.findViewById(R.id.addition_button);
+            this.mSubtractionButton = albumItemView.findViewById(R.id.subtraction_button);
             this.mAdapter = adapter;
             this.mAdditionButton.setOnClickListener(this);
             this.mSubtractionButton.setOnClickListener(this);
@@ -113,60 +113,101 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
 
         /**
          * Identifies actions based on the button that was clicked.
-         * @param view The View of the specific button.
+         * @param view The View of the clicked button.
          */
         @SuppressLint({"SetTextI18n", "DefaultLocale"})
         @Override
         public void onClick(View view) {
 
+            //According to the item's ID,
             switch (view.getId()) {
 
-                case R.id.adding_button: {
+                //If the addition button was clicked, proceed with the following procedure:
+                case R.id.addition_button: {
+
+                    //Determine the position based on the loaded item.
                     int position = getLayoutPosition();
+
+                    //Determine the album's item based on the position.
                     AlbumItem product = mAlbumList.get(position);
+
+                    //Convert the quantity to an Integer value, increment the value and store the result in a variable.
                     int updatedAddedQuantity = Integer.parseInt(product.getAlbumQuantity()) + 1;
+
+                    //Set the album item's quantity with a String value of the updated quantity value.
                     product.setAlbumQuantity(String.valueOf(updatedAddedQuantity));
 
+                    //Determine the price of the album, extracting only a numeric result and ignoring the dollar sign ($).
                     String printedPrice = product.getAlbumPrice().substring(1);
+
+                    //Convert the price to a Double value based on the String result.
                     double addedPrice = Double.parseDouble(printedPrice);
 
+                    //Calculate the subtotal based on the numeric versions of the price and quantity.
                     double addedSubtotal = addedPrice * updatedAddedQuantity;
+
+                    //Set the album item's subtotal with a formatted String value of the calculated subtotal.
                     product.setAlbumSubtotal(String.format("$%.2f", addedSubtotal));
 
+                    //Set the TextView attribute with the new quantity of the album item.
                     if (this.mAlbumQuantity != null)
                         this.mAlbumQuantity.setText(String.valueOf(updatedAddedQuantity));
 
+                    //Set the TextView attribute with the new subtotal of the album item.
                     if (this.mAlbumSubtotal != null)
                         this.mAlbumSubtotal.setText(String.format("$%.2f", addedSubtotal));
 
+                    //Print a log message to mention that an album has been added.
                     Log.d(ADAPTER_LOG_TAG, "Added " + product.getAlbumQuantity() + " " + product.getAlbumTitle() + " Album(s)");
                 }
                 break;
 
+                //If the subtraction button was clicked, proceed with the following procedure:
+                case R.id.subtraction_button: {
 
-                case R.id.minus_button: {
+                    //Determine the position based on the loaded item.
                     int position = getLayoutPosition();
+
+                    //Determine the album's item based on the position.
                     AlbumItem product = mAlbumList.get(position);
+
+                    //Convert the quantity to an Integer value, decrement the value and store the result in a variable.
                     int updatedSubtractedQuantity = Integer.parseInt(product.getAlbumQuantity()) - 1;
+
+                    //Set the album item's quantity with a String value of the updated quantity value.
                     product.setAlbumQuantity(String.valueOf(updatedSubtractedQuantity));
 
+                    //If the quantity is less than 0, proceed with the following procedure:
                     if (updatedSubtractedQuantity < 0) {
+
+                        //Initialize the quantity to 0.
                         updatedSubtractedQuantity = 0;
+
+                        //Set the album item's quantity with a String value of 0.
                         product.setAlbumQuantity(String.valueOf(updatedSubtractedQuantity));
                     }
 
+                    //Determine the price of the album, extracting only a numeric result and ignoring the dollar sign ($).
                     String printedPrice = product.getAlbumPrice().substring(1);
+
+                    //Convert the price to a Double value based on the String result.
                     double subtractedPrice = Double.parseDouble(printedPrice);
 
+                    //Calculate the subtotal based on the numeric versions of the price and quantity.
                     double subtractedSubtotal = subtractedPrice * updatedSubtractedQuantity;
+
+                    //Set the album item's subtotal with a formatted String value of the calculated subtotal.
                     product.setAlbumSubtotal(String.format("$%.2f", subtractedSubtotal));
 
+                    //Set the TextView attribute with the new quantity of the album item.
                     if (this.mAlbumQuantity != null)
                         this.mAlbumQuantity.setText(String.valueOf(updatedSubtractedQuantity));
 
+                    //Set the TextView attribute with the new subtotal of the album item.
                     if (this.mAlbumSubtotal != null)
                         this.mAlbumSubtotal.setText(String.format("$%.2f", subtractedSubtotal));
 
+                    //Print a log message to mention that an album has been subtracted.
                     Log.d(ADAPTER_LOG_TAG, "Subtracted " + product.getAlbumQuantity() + " " + product.getAlbumTitle() + " Album(s)");
                 }
                 break;
